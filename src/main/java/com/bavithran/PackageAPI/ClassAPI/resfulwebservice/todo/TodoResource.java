0c51cb1c7bb23db35f481e5a276f45a858dd8f38
bavithran durai <duraibavi14@gmail.com>
2021-06-11 12:28:49 +0530

    Add files via upload

--- src/main/java/com/bavithran/PackageAPI/ClassAPI/resfulwebservice/todo/TodoResource.java
+++ src/main/java/com/bavithran/PackageAPI/ClassAPI/resfulwebservice/todo/TodoResource.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@CrossOrigin(origins="https://avithran.github.io")
+@CrossOrigin
 public class TodoResource {
 
 	@Autowired
